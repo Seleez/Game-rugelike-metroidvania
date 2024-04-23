27a60e5dc458e080cdb055894f0b0e42eef8167b
Seleez <bartekczernek10@gmail.com>
2024-04-23 18:13:10 +0200

    Jump working(wip duble jump)

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -4,6 +4,11 @@ var input
 @export var speed = 100.0
 @export var gravity = 10
 
+@export var jump_force = 500
+var jump_count = 0
+@export var max_jump = 2
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,9 +32,26 @@ func movement(delta):
 			$Sprite2D.flip_h = true
 	elif input == 0:
 		velocity.x = 0
+	#TODO Jump Animation
+	if is_on_floor():
+		jump_count = 0
+	
+	if Input.is_action_pressed("ui_accept") && is_on_floor() && jump_count < max_jump:
+		velocity.y -= jump_force
+		velocity.x = input
+		jump_count += 1
+	if Input.is_action_just_pressed("ui_accept") && !is_on_floor() && jump_count < max_jump:
+		velocity.y -= jump_force
+		velocity.x = input
+		jump_count += 1
+	if Input.is_action_just_released("ui_accept") && !is_on_floor() && jump_count < max_jump:
+		velocity.y = gravity
+		jump_count += 1
 	gravity_force()
-	
 	move_and_slide()
+
 	
+
+
 func gravity_force():
 	velocity.y += gravity
