94b0211754e910f9cc3fd149a77001633597976f
Seleez <bartekczernek10@gmail.com>
2024-04-26 23:08:52 +0200

    Horizontal spread added
    Effecting the horizontallness of dungeon

--- Game/Scripts/Level Generation/Main.gd
+++ Game/Scripts/Level Generation/Main.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 var Room = preload("res://Scenes/Room.tscn")
 
-var tile_size = 32
-var num_rooms = 50
-var min_size = 4
-var max_size = 10
+@export var tile_size = 32
+@export var num_rooms = 50
+@export var min_size = 4
+@export var max_size = 10
+@export var horizontal_spread = 4000
 
 func _ready():
 	randomize()
@@ -13,7 +14,7 @@ func _ready():
 	
 func make_rooms():
 	for i in range(num_rooms):
-		var position = Vector2(0,0)
+		var position = Vector2(randf_range(-horizontal_spread, horizontal_spread),0)
 		var room = Room.instantiate()
 		var width = min_size + randi() % (max_size - min_size)
 		var height = min_size + randi() % (max_size - min_size)
